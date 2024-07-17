e92647883af0b70663e6eb802d1de37ef77d3acf
jinxing <jinxing@lixiang.com>
2024-07-17 16:29:35 +0800

    feature: multiple threads to  mulitiply mitrix
    Flac codec added which is a prerequisite of enabling flac support
    for audio player
    
    Issue-Id: DUMMY-xxxx, DUMMY-xxxx

--- src/matrix.rs
+++ src/matrix.rs
@@ -2,9 +2,15 @@ use core::fmt;
 use std::{
     fmt::{Debug, Display},
     ops::{Add, AddAssign, Mul},
+    sync::mpsc,
+    thread,
 };
 
 use anyhow::{anyhow, Result};
+
+use crate::{dot_product, Vector};
+
+const NUM_THREADS: usize = 4;
 
 pub struct Matrix<T: Debug> {
     data: Vec<T>,
@@ -12,21 +18,80 @@ pub struct Matrix<T: Debug> {
     col: usize,
 }
 
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    value: T,
+}
+
+pub struct Msg<T> {
+    input: MsgInput<T>,
+    //sender to send back the result
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
 where
-    T: Debug + Add<Output = T> + AddAssign + Mul<Output = T> + Copy + Default,
+    T: Debug + Add<Output = T> + AddAssign + Mul<Output = T> + Copy + Default + Send + 'static,
 {
     if a.col != b.row {
         return Err(anyhow!("invalid matrix size"));
     }
+
+    let senders = (0..NUM_THREADS)
+        .map(|_| {
+            let (tx, rx) = mpsc::channel::<Msg<T>>();
+            let tx = tx.clone();
+            thread::spawn(move || {
+                for msg in rx {
+                    let value = dot_product(msg.input.row, msg.input.col)?;
+                    if let Err(e) = msg.sender.send(MsgOutput {
+                        idx: msg.input.idx,
+                        value,
+                    }) {
+                        eprintln!("send back result failed,e:{}", e);
+                    }
+                }
+                Ok::<_, anyhow::Error>(())
+            });
+            tx
+        })
+        .collect::<Vec<_>>();
+
+    //generate threads which receive the input and send back a sender to send back the result
+
     let mut data = vec![T::default(); a.row * b.col];
+    let mut receivers = Vec::with_capacity(a.row * b.col);
     for i in 0..a.row {
         for j in 0..b.col {
-            for k in 0..a.col {
-                data[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let col_data = b.data[j..]
+                .iter()
+                .step_by(b.col)
+                .copied()
+                .collect::<Vec<_>>();
+            let idx = i * b.col + j;
+            let col = Vector::new(col_data);
+            let input = MsgInput::new(idx, row, col);
+            let (sender, receiver) = oneshot::channel();
+            let msg = Msg::new(input, sender);
+            if let Err(e) = senders[idx % NUM_THREADS].send(msg) {
+                eprintln!("send input failed,e:{}", e);
             }
+            receivers.push(receiver);
         }
     }
+
+    for receiver in receivers {
+        let output = receiver.recv()?;
+        data[output.idx] = output.value;
+    }
+
     Ok(Matrix {
         data,
         row: a.row,
@@ -76,16 +141,28 @@ where
     }
 }
 
+impl<T> MsgInput<T> {
+    fn new(idx: usize, row: Vector<T>, col: Vector<T>) -> Self {
+        Self { idx, row, col }
+    }
+}
+
+impl<T> Msg<T> {
+    fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Self { input, sender }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_matrix() -> Result<()> {
-        let a = Matrix::new(vec![1, 2, 3, 4], 2, 2);
-        let b = Matrix::new(vec![1, 2, 3, 4], 2, 2);
+        let a = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new(vec![7, 8, 9, 10, 11, 12], 3, 2);
         let c = multiply(&a, &b).unwrap();
-        assert_eq!(format!("{:?}", c), "Matrix(2x2):{7 10},{15 22}");
+        assert_eq!(format!("{:?}", c), "Matrix(2x2):{58 64},{139 154}");
         Ok(())
     }
 }
